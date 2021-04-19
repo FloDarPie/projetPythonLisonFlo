fa01ae35767b335f2356f93cb379ae2739a09401
lizzie222888 <lison.toulet@laposte.net>
2021-04-19 17:29:01 +0200

    MAJ 17.28

--- code/prof.py
+++ code/prof.py
@@ -1,6 +1,14 @@
 from tkinter import *
 import os
 
+#voiture de 2 à 8 et camion de 9 à 13
+M=[[0,0,0,4,4,12]
+,[0,0,0,0,3,12]
+,[0,0,1,1,3,12]
+,[0,0,9,11,11,11]
+,[2,0,9,0,0,0]
+,[2,0,9,10,10,10]]
+
 
 path=os.getcwd()
 print(path)
@@ -10,20 +18,72 @@ path+="/assets/"
 print(path)
 LARG=600
 HAUT=600
+AUTRE=LARG/6
 DIM=LARG
 
 fen=Tk()
+fen .title("Parking")
 canv=Canvas(fen, width=LARG, height=HAUT, background="gray")
 canv.pack(padx=10, pady=10)
 
+#sortie du parking
 canv.create_line((LARG,2*(LARG/6)),(LARG,3*(LARG/6)), fill="red", width=9)
 
-#convertir image
+#ajouter image background
 parking=PhotoImage(file=path+"images/parking_fond.png")
 
-#affiche une image par rapport à son centre
+#affiche background par rapport à son centre
 centre=(DIM/2,DIM/2)
 canv.create_image(centre, image=parking)
+
+comp=0
+n=len (M)
+n2=len(M[0])
+#Placer la voiture qui sort
+
+for i in range (n):
+    L=M[i]
+    n2=len (L)
+    for j in range (n2):
+        if L[j]==1 and comp==0:
+            comp+=1
+            print(i,j)
+            voitureBouge=canv.create_rectangle(j*AUTRE,i*AUTRE,j*AUTRE+2*AUTRE,i*AUTRE+AUTRE,fill="magenta")
+
+
+#récupérer la position de la voiture entière
+def position(M,val):
+    XY=[]
+    if val != 0:       
+        for j in range(len(M)):
+            for i in range (len(M[0])):
+                if M[j][i] == val:
+                    XY.append([i,j])
+        return XY
+
+print(position(M,1))
+
+#placer les voitures/camions de la matrice
+def affichage(M) :
+    L={}
+    for i in range(n):
+        for j in range(n2):
+            if M[i][j] !=0 and M[i][j]!=1:
+                try:
+                    L[M[i][j]]==1
+                except:
+                    L[M[i][j]]=1                
+                    XY=position(M,M[i][j])
+                    print(XY[0][0])
+                    canv.create_rectangle(AUTRE*XY[0][0],AUTRE*XY[0][1],AUTRE*((XY[-1][0])+1),AUTRE*((XY[-1][1])+1),fill="red")
+                    canv.create_rectangle(AUTRE*XY[0][0],AUTRE*XY[0][1],AUTRE*((XY[-1][0])+1),AUTRE*((XY[-1][1])+1))
+
+    
+affichage(M)
+
+
+#RECTANGLE : canv.create_rectangle(x,y,x1,y1,fill="magenta")
+#ENLEVER RECTANGLE : 
 
 fen.mainloop()
 
