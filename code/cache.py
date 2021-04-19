7bc72cd8c1630490de3de1d972f4a1cdc62fc87f
FlorianDuzes <florian.duzes@gmail.com>
2021-04-19 13:52:03 +0200

    essai d'enregisteur

--- code/cache.py
+++ code/cache.py
@@ -6,12 +6,25 @@ path=path[:-4]
 path+="data/data.txt"
 
 
-def niveau():
+def lecteur():
     with open(path,'r') as data:
         num=int(data.readline(2),base=10)
         for i in range(num-1):
             data.readline(200)
         return data.readline(200)
-        
+
+matrice=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+
+def enregistreur(matrice):
+    contenu=""
+    with open(path,'r') as data:
+        contenu=data.read()
+    print(contenu)
+    contenu=contenu[2000:]
+    contenu+=str(matrice)
+    with open(path,'w') as data:
+        data.write(contenu)
+    
+enregistreur(matrice)
 
 print("fin")
