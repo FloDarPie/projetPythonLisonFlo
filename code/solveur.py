8613bda6d794dcb6e400172fc5032f4074e19aae
lizzie222888 <lison.toulet@laposte.net>
2021-05-16 22:13:17 +0200

    MAJ

---
+++ code/solveur.py
@@ -0,0 +1,34 @@
+from cache import *
+
+M=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,1,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+
+graph = {}
+
+visited = []
+queue = []
+
+def remplir(M):
+    L=[]
+    for i in range (6):
+        for j in range(6):
+            L.append(deplacement(M,[i,j]))
+    graph[str(M)]=L
+    for x in L:
+        return remplir(x)
+        
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+bfs(visited, graph, 'A')
