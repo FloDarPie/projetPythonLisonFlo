1b2287fc8449cca02a1ab1041d4724fba00f7636
FloDarPie <flo.duzes@gmail.com>
2021-06-11 14:43:31 +0200

    premier jeu

--- FloodIt/versionFlash/afficheur.py
+++ FloodIt/versionFlash/afficheur.py
@@ -20,53 +20,73 @@ class Affichage:
         #prendre les infos de la fenêtre
         self.ecranLargeur = root.winfo_screenwidth()
         self.ecranHauteur = root.winfo_screenheight()
-        print(self.ecranHauteur)
         
         self.canvas=tk.Canvas(root,width=self.ecranLargeur,height=self.ecranHauteur,borderwidth=0,bg="Black")
         
         self.canvas.grid(column=0, row=0, ipadx=0, ipady=0, sticky=tk.E+tk.N)
         
         self.titre = tk.PhotoImage(file="titre.png")
-        self.canvas.create_image((self.ecranLargeur//2,120),image=self.titre) #fixer le titre au milieu de l'écran
+        self.canvas.create_image((self.ecranLargeur//2,80),image=self.titre) #fixer le titre au milieu de l'écran
         
         #affichage respectif
         self.afficheAutre()
         self.afficheJeu(self.moteur.matrice)
+        self.canvas.bind("<Button-1>",self.clic)
         
         
     def afficheJeu(self,matrice):
         '''
-        ecranHauteur|ecranLargeur       ecranHauteur|ecranLargeur+800
+        depart | 157                        depart+X | 157
         
         
                                 (zone de jeu)
         
         
-        ecranHauteur+800|ecranLargeur   ecranHauteur+800|ecranLargeur+800
+        depart | 157 + X                    depart+X | 157+X
+        
+        
+        depart = self.ecranLargeur//2-self.ecranHauteur*3/8
+        
+        X = self.ecranHauteur * 3/4
         '''
-        #zone de jeu 800*800
-        angleDroitY = 237
-        cote = int(800 / self.moteur.taille) # format de la fenêtre de jeu
+        #zone de jeu
+        angleDroitY = 157
+        self.cote = int(self.ecranHauteur * 3/4 / self.moteur.taille) # format de la fenêtre de jeu
         
         i = 0 #indice de la cellule de la matrice
 
         for ligne in range(self.moteur.taille):
-            angleDroitX = self.ecranLargeur//2-400
+            angleDroitX = self.ecranLargeur//2-self.ecranHauteur*3/8
             for cell in range(self.moteur.taille):
-                self.canvas.create_rectangle((angleDroitX,angleDroitY), (angleDroitX+cote,angleDroitY+cote), fill = self.couleur[self.moteur.matrice[i]], width=0)
-                angleDroitX+=cote
+                self.canvas.create_rectangle((angleDroitX,angleDroitY), (angleDroitX+self.cote,angleDroitY+self.cote), fill = self.couleur[self.moteur.matrice[i]], width=0)
+                angleDroitX+=self.cote
                 i+=1
-            angleDroitY+=cote
+            angleDroitY+=self.cote
         
         
     def afficheAutre(self):
+        '''
         #BOUTON QUITTER
         self.boutton_quitter=None
         self.root.protocol("WM_DELETE_WINDOW",self.quitter)
 
         self.boutton_quitter=tk.Button(self.canvas,borderwidth=0,bg="Red",activebackground="Blue",command=self.quitter,cursor="X_cursor").place(x=10,y=10)
+        '''
+        self.canvas.create_text(100,self.ecranHauteur//2+200, text="Press 'Echap' to quit", fill='white', font="Arial 14 roman")
+        
+        self.canvas.create_text(140,self.ecranHauteur//2+250, text="Click on case to select a color. \nYou begin on top left.", fill='white', font="Arial 14 roman")
+        
     
-    
+    def clic(self,event):
+        a = (event.x,event.y)
+        
+        X = int((a[0]-self.ecranLargeur//2-self.ecranHauteur*3/8) // self.cote)+self.moteur.taille
+        Y = int((a[1]-157) // self.cote)
+        
+        if 0 <= X <= self.moteur.taille-1 and 0 <= Y <= self.moteur.taille-1:
+            cell = X+Y*self.moteur.taille
+            self.moteur.transform(self.moteur.matrice[cell])
+            self.afficheJeu(self.moteur.matrice)
     
     def quitterEchap(self,event):
         self.quitter()
